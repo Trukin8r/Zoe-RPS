de022867df54eafa9e013281a785f8b5440dfb34
Zoe Kimberly <175645375+ZoeKimberly@users.noreply.github.com>
2024-12-21 08:08:00 -0600

    lots of errors fixed

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+function getComputerChoice() {
+    let ff = Math.floor(Math.random() * 3);
+    switch(ff) {
+        case 0:
+            return "rock"
+            break;
+        case 1:
+            return "paper"
+            break;
+        case 2:
+            return "scissors"
+            break;
+        default: 
+            return "error pls click the button again"
+    }
+}
+
+console.log(getComputerChoice())
+
